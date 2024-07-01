24634c68e91b279589fb14e5d76d021e199a1685
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-07-01 14:39:45 +0300

    chore:🌻 add icons

--- apps/pauker/src/app/feature/auth/auth.component.ts
+++ apps/pauker/src/app/feature/auth/auth.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
