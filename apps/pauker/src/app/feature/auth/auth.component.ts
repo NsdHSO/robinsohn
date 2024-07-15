db3482d3a856825ffc5da9def829cd71175ba157
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2024-07-15 10:29:23 +0300

    feat: add api to test merge map

--- apps/pauker/src/app/feature/auth/auth.component.ts
+++ apps/pauker/src/app/feature/auth/auth.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
+import { Title } from '@angular/platform-browser';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [CommonModule, MatIcon],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss',
+  styleUrl: './auth.component.scss',providers:[AuthService]
 })
-export class AuthComponent {}
+export class AuthComponent implements OnInit {
+  /**
+   * Title Service
+   */
+  readonly titleService = inject(Title)
+
+  /**
+   * Auth service
+   */
+  readonly authService = inject(AuthService);
+
+  ngOnInit(){
+    this.titleService.setTitle('Auth Pauker Application')
+  }
+}
